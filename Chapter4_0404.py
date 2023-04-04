e2313f7452141a61dccc529ce5742354681ccee5
김지언 <werrt871@gmail.com>
2023-04-04 11:27:01 +0900

    good

--- Chapter4_0404.py
+++ Chapter4_0404.py
@@ -9,8 +9,12 @@ if 조건:
 else:
     실행문0   
 '''
+
+'''
 #오전? 오후?
-hour = 3
+hour_str = input("현재 시간은?)
+hour = int(hour_str)
+
 if hour>12 :
     print("12시가 지났으니 오후 입니다!")
 else :
@@ -24,3 +28,31 @@ elif hour < 18 :
     print("12시가 지나고 18시가 안지났으니 오후 입니다^^")    
 else :
     print("18시가 지났으니 저녁 입니다!")   
+'''
+
+#장학금 
+#score < 200 50만원 줌
+#200 >= score < 400 100만원 줌
+#400 >= score < 500 10000만원 줌
+
+'''score_str = input("점수는? ")
+score = int(score_str)
+
+if score < 200 :
+    print("50만원 획득")
+elif score < 400 :
+    print("100만원 획득")
+else :
+    print("10000만원 획득")
+'''
+
+#점심메뉴
+answer = input("오늘 학식 먹을래?")
+if answer == 'yes' :
+    print("8호관 1층 ㄱㄱ")
+else :
+    again_answer = input("떡볶이?")
+    if again_answer == 'yes' :
+        print("5호관 3층")
+    else :
+        print("걍 아무것도 먹지마")
