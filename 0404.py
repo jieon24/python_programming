12fa4fb73f42fc0932fd298e02952a83e5bf36a1
김지언 <werrt871@gmail.com>
2023-04-04 10:42:41 +0900

    new

--- 0404.py
+++ 0404.py
@@ -2,7 +2,7 @@
 
 '''str = "파이썬은 파이썬은 파이썬은 파이썬은 파이썬은 파이썬은"
 newstr = "파이썬은,하하하, 호호호 ,히히히, 파이썬은, 파이썬은"
-print(str.split())'''
+print(str.split())
 
 print(2,"+",3,"=",5)
 print('{}+{}={}'.format(2,3,5))
@@ -16,7 +16,7 @@ a,b = 5,6.000
 print('{0}+{1}={2}'.format(a,b,a+b))
 print('{0:d}+{1:f}={2:f},{3:s}'.format(a,b,a+b,"메롱"))
 
-'''
+
 print(str.find("파이썬"))
 print(str.find("파"))
 print(str.find("썬"))
@@ -28,3 +28,12 @@ print(str.index("자바"))
 newstr = str.replace("파이썬","자바")
 print("str : ")
 '''
+
+value = False #True
+print(type(value))
+print(int(value))
+
+print(bool(0), bool(1), bool(1.343), bool(-12))
+print(bool("dnauwk"),bool("-1"),bool(""))
+print( 'bool("")',bool(""))
+
