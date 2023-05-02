48a9db4b33f376d06055a61bb032740a2ad61294
김지언 <werrt871@gmail.com>
2023-05-02 12:30:45 +0900

    good

--- 0502.py
+++ 0502.py
@@ -42,3 +42,93 @@ def mysum2(num1, num2) :
 
 result = mysum2(100,2000)
 print(result)
+
+#지역변수, 전역변수
+num = 100 #전역 변수 global variable
+print("***num : ", num)
+
+def addone() :
+    num = 10
+    print("addone()", num + 1)
+    print("addone() num", num)
+    num = num * 8  + 20
+    return num
+
+addone()
+print("***num : ", num)
+
+#변수를 수정하지 않으면 내부 function에서도 전역변수 사용 가능
+num = 100 #전역 변수 global variable
+print("***num : ", num)
+
+def addone() :
+    #내부 function 에서 num을 수정하지 않는다
+    #print("addone()", num + 1)
+    #print("addone() num", num)
+    #num = num * 8  + 20
+    return num
+
+addone()
+print("***num : ", num)
+
+#내부 function에서 전역변수를 사용, 
+# 수정도 사용,function이 끝나도 그 값이 반영이 되면 좋겠음
+num = 100 #전역 변수 global variable
+print("***num : ", num)
+
+def addone() :
+    global num 
+    num = num + 1 #전역변수를 쓰겠다. 변수에 새로운 값 대입가능
+
+addone()
+print("***num : ", num)
+
+#인자의 갯수가 여러개인 경우
+print()
+print(1,2,3,6,8,9,...,16)
+
+print("=================================")
+def hello2(*names) : #hello2 찍기
+    for i in names:
+        print("hello",i)
+
+hello2("James", "Tom", "Pane", "Jennie")
+
+#합을 구하는 function
+print("=================================")
+def sum22(*numbers) :
+    result2 = 0
+    for num in numbers:
+        result2 = result2 +num
+    return result2
+
+print("합은 :",sum22(1,2,1,2,1,2,4,6,4,3,74,9))
+lis1 = [1,2,1,2,1,2,4,6,4,3,74,9]
+print(sum22(*lis1))
+
+#dictionary = {key:vlaue1, key2:vlaue2}
+print("=================================")
+coffee = {"americano":3300, "latte    ":3800, "tea      ":2500}
+
+def printmenu(**keyvalue) :
+    for key in keyvalue :
+        print(key, keyvalue[key])
+
+printmenu(**coffee)
+print("=================================")
+printmenu(americano=3300, latte=3800, tea=2500, hotdog=3500, sandwich=4500)
+
+print("=================================")
+def fun1(*num, **menu) :
+    result = 0
+    for n in num:
+        result = result + n
+    return result
+
+    for key in menu :
+        print(key, menu[key])
+
+coffee = {"americano":3300, "latte    ":3800, "tea      ":2500}
+fun1(1,2,4,6,8,2,3,4,3,21,2,2,1,1,1,americano=3300, latte=3800, tea=2500)
+fun1(*lis1, **coffee)
+print(fun1(1,2,4,6,8,2,3,4,3,21,2,2,1,1,1,**coffee))
