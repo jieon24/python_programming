84d608c4f9b306d90ee375dc8815bd2ac3e3229e
김지언 <werrt871@gmail.com>
2023-05-09 11:29:27 +0900

    ed

---
+++ 0509/hello_0509.py
@@ -0,0 +1,11 @@
+
+def helloworld():
+    print("hello world")
+
+if __name__  == "__main__":
+    print("__name__:",__name__)
+    print("Today is Thesday")
+    print("sunny")
+    helloworld()
+else :
+    print("hello_0509.py __name__:",) 
